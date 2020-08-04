22c2236fe4e84022f10b79b7d1ef3f13e3f63b9c
Najeer <najeerahammad.m@transneuron.com>
2020-08-04 09:44:17 +0530

    filters is added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,6 +8,21 @@ import { SharedService } from '../services/shared.service';
 })
 export class ProductListComponent implements OnInit {
   productList: any;
+  colorArray = [
+    { "colorName": "grey" },
+    { "colorName": "red" },
+    { "colorName": "orange" },
+    { "colorName": "yelow" },
+  ];
+
+  brandArray = [
+    { "colorName": "B1" },
+    { "colorName": "B2" },
+    { "colorName": "B3" },
+    { "colorName": "B4" },
+  ];
+  priceArray = [100, 200, 300];
+  discountArray = [10, 20, 30];
 
   constructor(private sharedService: SharedService) { }
 
@@ -20,6 +35,7 @@ export class ProductListComponent implements OnInit {
       console.log('reessss', res);
       if (res && res.length > 0) {
         this.productList = res;
+        // this.colorArray.push(res.colour)
       }
     });
   }
